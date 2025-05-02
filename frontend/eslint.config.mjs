3fa61290ba0f4815abbaa09d105b4639afde0e92
armogedonregis <armag171691@gmail.com>
2025-05-02 17:35:45 +0300

    fixed

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -16,7 +16,9 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/ban-ts-comment": "off"
+      "@typescript-eslint/ban-ts-comment": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off"
     },
   },
 ];
